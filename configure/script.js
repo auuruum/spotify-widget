4ec5e53203a746ff1025b3139470360bb45d9b5d
auuruum <aurimasdovgialo@gmail.com>
2025-04-20 01:04:12 +0300

    Update script.js

--- configure/script.js
+++ configure/script.js
@@ -7,7 +7,7 @@ const urlParams = new URLSearchParams(queryString);
 
 const code = urlParams.get("code") || "";
 
-const baseURL = "https://auuruum.github.io/spotify-widget/";
+const baseURL = "https://auuruum.github.io/spotify-widget";
 const redirect_uri = `${baseURL}/configure`;
 let refresh_token = "";
 let access_token = "";
